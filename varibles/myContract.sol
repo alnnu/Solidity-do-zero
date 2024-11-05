a467e0ca6f13e4f30367cca117df18b6f263e441
alnnu <luannf1020@gmail.com>
2024-11-05 16:36:10 -0300

    varibles

---
+++ varibles/myContract.sol
@@ -0,0 +1,32 @@
+// // SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+contract MyContract {
+    // State Varibles (save in block chain)
+    uint public myUint = 1; 
+    uint256 public myuINT256 = 1;
+    uint8 public myUint8 = 1;
+
+
+    string public text  = "hello word"; //see as a text
+    bytes32 public myBytes32 = "hello"; //see as an array
+
+    address public myAddress = 0x5B38Da6a701c568545dCfcB03FcB875f56beddC4; // A block chain address
+
+    //strucs
+    //define the struct
+    struct MyStruct {
+        uint id;
+        string name;
+    }
+    //crete the struct 
+    MyStruct public myStruct = MyStruct(1,"aaa");
+
+
+    // Local Varibles ( in a function )
+    function getValue() pure public returns(uint) {
+        // pure = dont modify the block chain
+        uint value = 1;
+        return value;
+    }
+}
